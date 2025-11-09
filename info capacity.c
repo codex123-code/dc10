e6be6e7f80157a6b858e412e10d4772036517009
codex123-code <nanawaresahil2@gmail.com>
2025-11-09 22:44:27 +0530

    Create info capacity.c

---
+++ info capacity.c
@@ -0,0 +1,29 @@
+clear;
+
+clear;
+
+clc;
+
+B-3.4*10 3,
+
+SNR-30
+
+SN-10^(SNR/10),
+
+C-B*log2(1+SN)//Information capacity
+
+printf("Information capacity of telephone is in C-%20
+
+OUTPUT:
+
+SNR =
+
+30.
+
+C =
+
+33888.569
+
+Information capacity of telephone is
+
+C = 33.89 kbps
